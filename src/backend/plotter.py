8d56603d84723d0d271191bc9fb91921a1244c9c
Akhand Pratap Tiwari <akdevstudios100@gmail.com>
2024-11-16 19:33:02 +0530

    Updated plotter added pie_chart plotter in class Plotter

--- src/backend/plotter.py
+++ src/backend/plotter.py
@@ -20,24 +20,24 @@ class Plotter:
                         ha='center', va='bottom')  # Centered horizontally, slightly above the bar
         plt.show()
 
-def plot_pie_chart(labels, data, figsize=(10, 10)):
-    # Calculate percentages
-    total = sum(data)
-    percentages = [f"{size/total*100:.1f}%" for size in data]
+    def plot_pie_chart(labels, data, figsize=(10, 10)):
+        # Calculate percentages
+        total = sum(data)
+        percentages = [f"{size/total*100:.1f}%" for size in data]
 
-    # Combine labels with data and percentages
-    labels_with_data = [f"{label}\n{size} ({percentage})" for label, size, percentage in zip(labels, data, percentages)]
+        # Combine labels with data and percentages
+        labels_with_data = [f"{label}\n{size} ({percentage})" for label, size, percentage in zip(labels, data, percentages)]
 
-    # Plotting a flat pie chart
-    plt.figure(figsize=figsize)
-    plt.pie(data, labels=None, startangle=90, counterclock=False) # Labels with indicating lines are manually added 
+        # Plotting a flat pie chart
+        plt.figure(figsize=figsize)
+        plt.pie(data, labels=None, startangle=90, counterclock=False) # Labels with indicating lines are manually added 
 
-    for i, (dataPoint, label) in enumerate(zip(data, labels_with_data)):
-        angle = 90 - (sum(data[:i]) + dataPoint / 2) / total * 360  # Calculate the angle
-        x = 1.2 * math.cos(angle * math.pi / 180)
-        y = 1.2 * math.sin(angle * math.pi / 180)
-        plt.text(x, y, label, ha='center', va='center', fontsize=10)
-        plt.plot([0, x/1.07], [0, y/1.07], linestyle='solid', linewidth=0.8)  # Line from center to text
+        for i, (dataPoint, label) in enumerate(zip(data, labels_with_data)):
+            angle = 90 - (sum(data[:i]) + dataPoint / 2) / total * 360  # Calculate the angle
+            x = 1.2 * math.cos(angle * math.pi / 180)
+            y = 1.2 * math.sin(angle * math.pi / 180)
+            plt.text(x, y, label, ha='center', va='center', fontsize=10)
+            plt.plot([0, x/1.07], [0, y/1.07], linestyle='solid', linewidth=0.8)  # Line from center to text
 
-    plt.axis('equal')  # Equal aspect ratio ensures the pie chart is a circle.
-    plt.show()
+        plt.axis('equal')  # Equal aspect ratio ensures the pie chart is a circle.
+        plt.show()
