1eb7fa70c2fa892db871076801f3fa20d55cd599
Akhand Pratap Tiwari <akdevstudios100@gmail.com>
2024-11-16 18:48:57 +0530

    Added pandas based analysis system TODO: Add exception handling

---
+++ src/backend/data_handler.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import glob
+from tabulate import tabulate
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+
+class DataHandler:
+    def format_ecl(self, df_ecl):
+        # Skip some rows that are not required
+        df_ecl_fmtd = df_ecl.iloc[8:].reset_index(drop=True)
+
+        # Split the text into separate with ';' as delimiter
+        df_ecl_fmtd = df_ecl_fmtd.iloc[:, 0].str.split(';', expand=True)
+
+        # Reassign columns
+        df_ecl_fmtd.columns = df_ecl_fmtd.iloc[0]
+
+        # Drop unrequired columns
+        df_ecl_fmtd = df_ecl_fmtd.drop(df_ecl_fmtd.columns[[0, -1]], axis=1)
+
+        # Reset the index
+        df_ecl_fmtd = df_ecl_fmtd.iloc[1:].reset_index(drop=True)
+
+        return df_ecl_fmtd
+
+    def format_ecf(self, df_ecf):
+        # Skip some rows that are not required
+        df_ecf_fmtd = df_ecf.iloc[1:].reset_index(drop=True)
+
+        # Split the text into separate with ';' as delimiter
+        df_ecf_fmtd = df_ecf_fmtd.iloc[:, 0].str.split(';', expand=True)
+
+        # Reassign columns
+        df_ecf_fmtd.columns = df_ecf_fmtd.iloc[0]
+
+        # Drop unrequired columns
+        df_ecf_fmtd = df_ecf_fmtd.drop(df_ecf_fmtd.columns[-1], axis=1)
+
+        # Reset the index
+        df_ecf_fmtd = df_ecf_fmtd.iloc[1:].reset_index(drop=True)
+
+        return df_ecf_fmtd
+
+    # Read and format dataframe from file_path provided
+    def read_csv_from_file(self, file_path):
+        data = pd.read_csv(file_path)
+
+        ecf_index = data[data.iloc[:, 0].str.contains(
+            "ERROR CODE FREQUENCY")].index[0]
+
+        df_ecl = data.iloc[0:ecf_index, ]
+        df_ecf = data.iloc[ecf_index:, ]
+
+        df_ecl = self.format_ecl(df_ecl)
+        df_ecf = self.format_ecf(df_ecf)
+
+        return df_ecl, df_ecf
+
+    # Read, format and merge dataframe from files under folder_path provided
+    def read_csv_from_folder(self, folder_path):
+        csv_files = glob.glob(f"{folder_path}/*.csv")
+        # print("Printing csv files", csv_files)
+        merged_df_ecl = pd.DataFrame()
+        merged_df_ecf = pd.DataFrame()
+
+        for csv_file_path in tqdm(csv_files, desc="Reading Files"):
+            df_ecl, df_ecf = self.read_csv_from_file(csv_file_path)
+            merged_df_ecl = pd.concat(
+                [merged_df_ecl, df_ecl])  # Deault axis = 0
+            merged_df_ecf = pd.concat(
+                [merged_df_ecf, df_ecf])  # Deault axis = 0
+
+        merged_df_ecf.reset_index(drop=True, inplace=True)
+        merged_df_ecl.reset_index(drop=True, inplace=True)
+
+        return merged_df_ecl, merged_df_ecf
+
+    def get_ecl_freq_summary(self, df_ecl_fmtd):
+        summary = df_ecl_fmtd.groupby(by=["Description"])
+        summary = summary.size().reset_index(name='Frequency')
+        summary['SortKey'] = summary['Description'].apply(lambda x: (-len(x), x.lower()))
+        summary = summary.sort_values(by="SortKey", ignore_index=True)
+        summary = summary.drop(columns='SortKey')
+        return summary
+
+    def import_folder(self, folder_path):
+        self.folder_path = folder_path
+        self.ecl = pd.Dataframe()
+        self.ecf = pd.DataFrame()
+        self.ecl, self.ecf = self.read_csv_from_folder(folder_path)
+        self.freq_summary = self.get_ecl_freq_summary(self.ecl)
+
+    def __init__(self, folder_path):
+        self.import_folder(folder_path)
---
+++ src/backend/plotter.py
@@ -0,0 +1,43 @@
+from matplotlib import pyplot as plt
+import seaborn as sns
+import math
+
+class Plotter:
+    def plot_bar_chart(x, y, figsize=(10, 3), xlabel="Errors", ylabel="Frequency"):
+        plt.figure(figsize=figsize)
+        plt.xlabel(xlabel=xlabel)
+        plt.ylabel(ylabel=ylabel)
+        plt.xticks(rotation=45, ha='right')
+        plt.tight_layout()
+
+        ax = sns.barplot(x=x, y=y)
+
+        for p in ax.patches:
+            bar_height = p.get_height()
+            bar_x_pos = p.get_x()
+            bar_width = p.get_width()
+            ax.annotate(int(bar_height), (bar_x_pos + bar_width / 2.0, bar_height),  # Position at top of bar
+                        ha='center', va='bottom')  # Centered horizontally, slightly above the bar
+        plt.show()
+
+def plot_pie_chart(labels, data, figsize=(10, 10)):
+    # Calculate percentages
+    total = sum(data)
+    percentages = [f"{size/total*100:.1f}%" for size in data]
+
+    # Combine labels with data and percentages
+    labels_with_data = [f"{label}\n{size} ({percentage})" for label, size, percentage in zip(labels, data, percentages)]
+
+    # Plotting a flat pie chart
+    plt.figure(figsize=figsize)
+    plt.pie(data, labels=None, startangle=90, counterclock=False) # Labels with indicating lines are manually added 
+
+    for i, (dataPoint, label) in enumerate(zip(data, labels_with_data)):
+        angle = 90 - (sum(data[:i]) + dataPoint / 2) / total * 360  # Calculate the angle
+        x = 1.2 * math.cos(angle * math.pi / 180)
+        y = 1.2 * math.sin(angle * math.pi / 180)
+        plt.text(x, y, label, ha='center', va='center', fontsize=10)
+        plt.plot([0, x/1.07], [0, y/1.07], linestyle='solid', linewidth=0.8)  # Line from center to text
+
+    plt.axis('equal')  # Equal aspect ratio ensures the pie chart is a circle.
+    plt.show()
