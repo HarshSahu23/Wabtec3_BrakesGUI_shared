beb4d1441b8f1df7369fa93d9479bdeb5c1c21c3
Akhand Pratap Tiwari <akdevstudios100@gmail.com>
2024-11-18 17:16:50 +0530

    Added functinality to read other type of csv as well More other log dmp csv is being read in dmp variables

--- src/backend/data_handler.py
+++ src/backend/data_handler.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import glob
 from tqdm import tqdm
+from enum import Enum
+import tabulate
+
+class FileClasses(Enum):
+    ECL_ECF = 1
+    DMP_LOG = 2
 
 class DataHandler:
     # __folder_path = ""
     # ecl = pd.DataFrame()
     # ecf = pd.DataFrame()
     # ecl_freq_summary = pd.DataFrame()
-
+    __file_classes = []
     def __format_ecl(self, df_ecl):
         # Skip some rows that are not required
         df_ecl_fmtd = df_ecl.iloc[8:].reset_index(drop=True)
@@ -44,8 +50,29 @@ class DataHandler:
 
         return df_ecf_fmtd
 
+
+    def get_file_class(self, file_path):
+        df = pd.read_csv(file_path, nrows=50)
+
+        file_type_and_col_names = {
+            FileClasses.ECL_ECF : ["ERROR CODE LISTING", "Code(hex)", "Ticks(hex)"],
+            FileClasses.DMP_LOG : ["MOD_TICK", "MONTIME"]
+        }
+
+        for column_name in df.columns:
+            for file_class, column_list in file_type_and_col_names.items():
+                for column in column_list:
+                    if(column_name.find(column) != -1):
+                        return file_class
+    
+    
+    # Error_log_dump_reader CSV reader
+    def read_dmp_from_csv_file(self, file_path):
+        data = pd.read_csv(file_path)
+        return data
+
     # Read and format dataframe from file_path provided
-    def read_csv_from_file(self, file_path):
+    def read_ecl_ecf_from_csv_file(self, file_path):
         data = pd.read_csv(file_path)
 
         ecf_index = data[data.iloc[:, 0].str.contains(
@@ -58,25 +85,30 @@ class DataHandler:
         df_ecf = self.__format_ecf(df_ecf)
 
         return df_ecl, df_ecf
+    
 
     # Read, format and merge dataframe from files under folder_path provided
     def read_csv_from_folder(self, folder_path):
         csv_files = glob.glob(f"{folder_path}/*.csv")
-        # print("Printing csv files", csv_files)
         merged_df_ecl = pd.DataFrame()
         merged_df_ecf = pd.DataFrame()
+        merged_dmp = pd.DataFrame()
 
         for csv_file_path in tqdm(csv_files, desc="Reading Files"):
-            df_ecl, df_ecf = self.read_csv_from_file(csv_file_path)
-            merged_df_ecl = pd.concat(
-                [merged_df_ecl, df_ecl])  # Default axis = 0
-            merged_df_ecf = pd.concat(
-                [merged_df_ecf, df_ecf])  # Default axis = 0
+            file_type = self.get_file_class(csv_file_path)
+            if(file_type == FileClasses.ECL_ECF):
+                df_ecl, df_ecf = self.read_ecl_ecf_from_csv_file(csv_file_path)
+                merged_df_ecl = pd.concat([merged_df_ecl, df_ecl])  # Default axis = 0
+                merged_df_ecf = pd.concat([merged_df_ecf, df_ecf])  # Default axis = 0
+            else:
+                df_dmp = self.read_dmp_from_csv_file(csv_file_path)
+                merged_dmp = pd.concat([merged_dmp, df_dmp])
 
         merged_df_ecf.reset_index(drop=True, inplace=True)
         merged_df_ecl.reset_index(drop=True, inplace=True)
+        merged_dmp.reset_index(drop=True, inplace=True)
 
-        return merged_df_ecl, merged_df_ecf
+        return merged_df_ecl, merged_df_ecf, merged_dmp
 
     def get_ecl_freq_summary(self, df_ecl_fmtd):
         summary = df_ecl_fmtd.groupby(by=["Description"])
@@ -86,15 +118,33 @@ class DataHandler:
         summary = summary.drop(columns='SortKey')
         return summary
 
+    def get_dmp_freq_summary(self, df_dmp):
+        summary = df_dmp.sum(axis=0)
+        return summary
+
     def get_folder(self):
         return self.__folder_path
+
+    def filter_dmp(self, df_dmp):
+        required_columns = ["FILL_1","VENT_1","FILL_2","VENT_2","FILL_3","VENT_3","FILL_4","VENT_4"]
+        f_df_dmp = df_dmp[required_columns]
+        f_df_dmp = f_df_dmp.loc[:, (f_df_dmp[required_columns] != 0).any(axis=0)]
+        return f_df_dmp
+
 
     def set_folder(self, folder_path):
         self.__folder_path = folder_path
         self.ecl = pd.DataFrame()
         self.ecf = pd.DataFrame()
-        self.ecl, self.ecf = self.read_csv_from_folder(self.__folder_path)
+        self.dmp = pd.DataFrame()
+        self.ecl, self.ecf, self.dmp = self.read_csv_from_folder(self.__folder_path)
         self.ecl_freq_summary = self.get_ecl_freq_summary(self.ecl)
+        self.filtered_dmp = self.filter_dmp(self.dmp)
+        self.dmp_freq_summary = self.get_dmp_freq_summary(self.filtered_dmp)
 
     def __init__(self, folder_path):
         self.set_folder(folder_path)
+
+if __name__ == "__main__":
+    folder_path = "csv"
+    dh = DataHandler(folder_path)
